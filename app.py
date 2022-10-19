8e8c9b7d1f3af1a7932148f5230282ffd8bfd81d
panda1 <blueblue0011@gmail.com>
2022-10-18 17:02:40 -0700

    week23 warmup 1 backend

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import request, Flask
 import json
 from dbhelpers import run_statement
 from apihelpers import check_endpoint_info
+import dbcreds as d
 
 app = Flask(__name__)
 
@@ -15,4 +16,13 @@ def get_restaurant():
         return "Sorry, there was an error getting the gifs"
 
 # FIX ME WITH PROPER RUNNING!
-app.run(debug=True)
+if(d.production_mode == True):
+    print("Running in Production Mode")
+    import bjoern #type:ignore
+    bjoern.run(app, "0.0.0.0", 5000)
+    app.run(debug=True)
+else:
+    from flask_cors import CORS
+    CORS(app)
+    print("Running in Testing Mode")
+    app.run(debug=True)
